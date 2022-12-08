9cb22cfa89c2131c7d186490a7ed48ae5e626431
Renan <renan.fachin@hotmail.com>
2022-12-08 15:53:30 -0300

    customize splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  return(
+    <View style={style.container}>
+      <Text>
+        Olá!
+      </Text>
+
+      <Text style={{fontWeight: 'bold'}}>
+        NLW#05
+      </Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const style =StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }
+})
